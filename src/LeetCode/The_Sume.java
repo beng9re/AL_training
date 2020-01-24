d79a609fad7ddea6a724365d24bd3b21274fb74a
kbh0581 <kbh0581@gmail.com>
2020-01-24 19:07:34 +0900

    Modify : Map 으로 풀기 추가

--- src/LeetCode/The_Sume.java
+++ src/LeetCode/The_Sume.java
@@ -1,6 +1,8 @@
 package LeetCode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class The_Sume {
 
@@ -41,30 +43,69 @@ public class The_Sume {
     }
 
 
-    public void test(){
-        int [] testCase1 = {2,7,11,5};
-        int target1 = 9;
-        int [] testCase2 = {3,2,4};
-        int taget2 = 6;
+    /**
+    * 맵으로 풀기
+     * * */
+    public int[] mapTwo(int[] nums, int target){
+        Map<Integer,Integer> hash = new HashMap<Integer, Integer>();
 
+        for (int i = 0; i < nums.length ; i++) {
+            int val = target - nums[i];
 
+            if(hash.containsKey(val)){
+                return new int[]{hash.get(val),i};
+            }
 
-        if(Arrays.equals(twoSum(testCase1,target1),new int[]{0,1})
-            && Arrays.equals(twoSum(testCase2,taget2),new int[]{1,2})
-        ){
-            System.out.println("Test Suc");
-        }else{
+            hash.put(nums[i],i);
 
-            System.out.println(twoSum(testCase1,target1));
-            System.out.println(twoSum(testCase2,taget2));
-            System.out.println("fail");
         }
 
+        throw new IllegalArgumentException("No");
 
 
     }
 
 
+
+
+
+
+    public void test(){
+        int [] testCase1 = {2,7,11,5};
+        int target1 = 9;
+        int [] testCase2 = {3,2,4};
+        int target2 = 6;
+
+
+
+        if(Arrays.equals(twoSum(testCase1,target1),new int[]{0,1})
+            && Arrays.equals(twoSum(testCase2,target2),new int[]{1,2})
+        ){
+            System.out.println("Test Suc");
+        }else{
+            System.out.println("fail");
+            Arrays.stream(twoSum(testCase1, target1)).forEach(System.out::print);
+            System.out.println();
+            Arrays.stream(twoSum(testCase2, target2)).forEach(System.out::print);
+
+        }
+        System.out.println("map =>");
+
+        if(Arrays.equals(mapTwo(testCase1,target1),new int[]{0,1})
+                && Arrays.equals(mapTwo(testCase2,target2),new int[]{1,2})
+        ){
+            System.out.println("Test Suc");
+        }else{
+
+            System.out.println("fail");
+            Arrays.stream(mapTwo(testCase1, target1)).forEach(System.out::print);
+            System.out.println();
+            Arrays.stream(mapTwo(testCase1, target1)).forEach(System.out::print);
+
+        }
+
+
+    }
 
 
 }
